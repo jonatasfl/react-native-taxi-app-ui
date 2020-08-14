0c24a3cebacc863744938d2ac723beb1d786eb2f
Jonatas Lizandro <jonatas.exe@gmail.com>
2020-08-14 15:11:43 -0300

    fix eslint problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,14 @@ module.exports = {
   env: {
     es2020: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    'plugin:@typescript-eslint/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -11,11 +18,23 @@ module.exports = {
     ecmaVersion: 11,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
   rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "ts": "never",
+        "tsx": "never"
+      }
+    ],
     'prettier/prettier': 'error',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-filename-extension": [1, { "extensions": [".tsx"] }],
+    "import/prefer-default-export": "off",
     'react/jsx-filename-extension': [
       'warn',
       {
@@ -24,5 +43,10 @@ module.exports = {
     ],
     'default-case': 'off',
     'no-use-before-define': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    },
   },
 };
--- App.tsx
+++ App.tsx
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Index from './src/index';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App: React.FC = () => {
+  return <Index />;
+};
+
+export default App;
