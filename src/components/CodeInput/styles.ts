47114b068cd259e3ec9c1fc002b9def35145c1f7
Jonatas Lizandro <jonatas.exe@gmail.com>
2020-08-31 21:17:57 -0300

    styled components theme in CodeInput and PhoneInput

--- src/components/CodeInput/styles.ts
+++ src/components/CodeInput/styles.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components/native';
 
-import { color } from '../../style-vars';
+import { ThemeProps } from '../../theme';
 
 export const Container = styled.View`
   align-items: center;
   justify-content: center;
   width: 55px;
   height: 55px;
-  border: 1px solid ${color.gray};
+  border: 1px solid ${({ theme }: ThemeProps) => theme.color.gray};
   border-radius: ${55 / 2}px;
 `;
 
@@ -16,5 +16,5 @@ export const Input = styled.TextInput.attrs({ keyboardType: 'numeric' })`
   width: 55px;
   height: 55px;
   font-size: 18px;
-  color: ${color.primary};
+  color: ${({ theme }: ThemeProps) => theme.color.primary};
 `;
--- src/components/PhoneInput/styles.ts
+++ src/components/PhoneInput/styles.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components/native';
 
-import { color } from '../../style-vars';
+import { ThemeProps } from '../../theme';
 
 export const Container = styled.View`
   flex-direction: row;
   align-items: center;
   width: 100%;
   height: 55px;
-  border: 1px solid ${color.gray};
+  border: 1px solid ${({ theme }: ThemeProps) => theme.color.gray};
   border-radius: 45px;
   padding: 0 24px;
   margin-bottom: 24px;
@@ -15,7 +15,7 @@ export const Container = styled.View`
 
 export const Code = styled.Text`
   font-size: 18px;
-  color: ${color.primary};
+  color: ${({ theme }: ThemeProps) => theme.color.primary};
   margin-right: 19px;
 `;
 
@@ -23,5 +23,5 @@ export const Input = styled.TextInput`
   font-size: 18px;
   width: 100%;
   height: 100%;
-  color: ${color.gray};
+  color: ${({ theme }: ThemeProps) => theme.color.gray};
 `;
